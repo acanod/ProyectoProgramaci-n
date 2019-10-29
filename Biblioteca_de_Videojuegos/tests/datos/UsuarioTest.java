5b3b72eafa828896a68614e7176f0d9135bcf0da
Imanol <imanololalde@opendeusto.es>
2019-10-29 20:36:00 +0100

    Tests

---
+++ Biblioteca_de_Videojuegos/tests/datos/UsuarioTest.java
@@ -0,0 +1,39 @@
+package datos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UsuarioTest {
+	private Usuario u;
+	@Before
+	public void setUp() throws Exception {
+		u= new Usuario("Nombre", "Apellido", "NM@gmail.com","Espanya", "nombreapellido33");
+	}
+
+	@Test
+	public void testGetNombre() {
+		assertEquals("Nombre",u.getNombre());
+	}
+
+	@Test
+	public void testGetApellido() {
+		assertEquals("Apellido",u.getApellido());
+	}
+	
+	@Test
+	public void testGetEmail() {
+		assertEquals("NM@gmail.com",u.getEmail());
+	}
+
+	@Test
+	public void testGetPais() {
+		assertEquals("Espanya",u.getPais());
+	}
+	
+	@Test
+	public void testGetPassword() {
+		assertEquals("nombreapellido33",u.getPassword());
+	}
+}
