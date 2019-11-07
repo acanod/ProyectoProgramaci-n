9decbd8d6e30188f69c4ef1d3ad5ff8a9403efde
Imanol <imanololalde@opendeusto.es>
2019-11-07 17:49:17 +0100

    -Mejora visual clase inicio

--- Biblioteca_de_Videojuegos/src/ventanas/V_inicio.java
+++ Biblioteca_de_Videojuegos/src/ventanas/V_inicio.java
@@ -8,6 +8,8 @@ import javax.swing.*;
 
 import base_de_datos.*;
 import datos.Usuario;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class V_inicio extends JFrame {
 
@@ -26,25 +28,11 @@ public class V_inicio extends JFrame {
 
 	private void componentes() {
 		// Contenedores
-		JPanel panelCentral = new JPanel(new FlowLayout());
-		JPanel panelInfo = new JPanel(new GridLayout(2, 0));
+		JPanel panelCentral = new JPanel();
 		JPanel panelBotones = new JPanel(new FlowLayout());
-		
-		//Componentes
-		JTextField nombre = new JTextField(35);
-		JPasswordField password = new JPasswordField(35);
-		JLabel lnombre = new JLabel("Nombre");
-		JLabel lpassword = new JLabel("Contraseña");
 		JButton bIniciar = new JButton("Iniciar sesión");
 		
-		bIniciar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Usuario iniciar = new Usuario(nombre.getText(), password.getPassword().toString());
-				BaseDeDatos.comprobarLogin(iniciar);
-				new V_principal(iniciar).setVisible(true);
-				V_inicio.this.setVisible(false);
-			}
-		});
+		
 		JButton bRegistrar = new JButton("Registrar");
 		bRegistrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -58,25 +46,60 @@ public class V_inicio extends JFrame {
 				V_inicio.this.dispose();
 			}
 		});
-
-		// Asignacion de componentes a contenedores
-		panelInfo.add(lnombre);
-		panelInfo.add(nombre);
-		panelInfo.add(lpassword);
-		panelInfo.add(password);
 		panelBotones.add(bIniciar);
 		panelBotones.add(bRegistrar);
 		panelBotones.add(bAtras);
-		panelCentral.add(panelInfo);
 
-		add(panelCentral, BorderLayout.CENTER);
-		add(panelBotones, BorderLayout.SOUTH);
+		getContentPane().add(panelCentral, BorderLayout.CENTER);
+		JLabel lpassword = new JLabel("Contraseña");
+		JPasswordField password = new JPasswordField(35);
+		JLabel lnombre = new JLabel("Nombre");
+		
+		//Componentes
+		JTextField nombre = new JTextField(100);
+		GroupLayout gl_panelCentral = new GroupLayout(panelCentral);
+		gl_panelCentral.setHorizontalGroup(
+			gl_panelCentral.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panelCentral.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panelCentral.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lnombre, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)
+						.addComponent(lpassword, GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panelCentral.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(nombre, 0, 0, Short.MAX_VALUE)
+						.addComponent(password, GroupLayout.DEFAULT_SIZE, 696, Short.MAX_VALUE))
+					.addGap(16))
+		);
+		gl_panelCentral.setVerticalGroup(
+			gl_panelCentral.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panelCentral.createSequentialGroup()
+					.addContainerGap(71, Short.MAX_VALUE)
+					.addGroup(gl_panelCentral.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lnombre, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+						.addComponent(nombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panelCentral.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lpassword, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+						.addComponent(password, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(41))
+		);
+		panelCentral.setLayout(gl_panelCentral);
+		getContentPane().add(panelBotones, BorderLayout.SOUTH);
 		
 		bRegistrar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				V_inicio.this.setVisible(false);
 				new V_registro();
+			}
+		});
+		bIniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Usuario iniciar = new Usuario(nombre.getText(), password.getPassword().toString());
+				BaseDeDatos.comprobarLogin(iniciar);
+				new V_principal(iniciar).setVisible(true);
+				V_inicio.this.setVisible(false);
 			}
 		});
 		
