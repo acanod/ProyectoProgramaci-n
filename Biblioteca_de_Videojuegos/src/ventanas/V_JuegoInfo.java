445173bb31d0b2e9d1466f089fb83bd91c04847f
Imanol <imanololalde@opendeusto.es>
2019-12-30 12:16:56 +0100

    Arreglo fallo

--- Biblioteca_de_Videojuegos/src/ventanas/V_JuegoInfo.java
+++ Biblioteca_de_Videojuegos/src/ventanas/V_JuegoInfo.java
@@ -41,13 +41,15 @@ public class V_JuegoInfo extends JFrame {
 		comprar.setEnabled(true);
 		contentPane.add(comprar, BorderLayout.SOUTH);
 		
-		for (int i = 0; i < u.getJuegosComprados().size(); i++) {
-			if ((u.getJuegosComprados().get(i).getNombre() == (j.getNombre()))) {
+		if(!(u.getJuegosComprados().size()==0)) {
+			for (int i = 0; i < u.getJuegosComprados().size(); i++) {
+			if ((u.getJuegosComprados().get(i).getNombre()== (j.getNombre()))) {
 				comprar.setVisible(false);
 				jugar.setEnabled(true);
 				contentPane.add(jugar, BorderLayout.SOUTH);
 			} 
-		}
+			}
+			}
 		
 		comprar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
